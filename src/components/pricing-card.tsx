fd3c19a8faf65d4ffb89f3d6ac7b877255f88f71
Achmad Ilmi Al Akbar <113961961+notilmi@users.noreply.github.com>
2024-09-15 11:06:18 +0700

    Fix Branding In Pricing Section

--- src/components/pricing-card.tsx
+++ src/components/pricing-card.tsx
@@ -1,7 +1,6 @@
 import { PricingSchema } from "@/lib/types";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { DoorOpen, Ghost } from "lucide-react";
 
 const PricingCard = ({
   currency,
@@ -11,18 +10,28 @@ const PricingCard = ({
   className,
 }: PricingSchema & { className: string }) => {
   return (
-    <div className="flex flex-wrap flex-row-reverse items-center justify-center bg-gray-100 rounded-xl shadow-lg hover:scale-105 cursor-pointer border transition-transform">
+    <div className="flex flex-wrap flex-row-reverse items-center justify-center  rounded-xl border">
       <div className={cn("mx-auto w-1/2  p-8", className)}>
         <h2 className="text-2xl font-bold mb-4">{type}</h2>
 
         <p className="text-5xl font-extrabold mb-2">
-          {currency}{harga}/mo
+          {currency}
+          {harga}/mo
         </p>
-        <p className="text-neutral-800">Fixed price<br />No minimum commitment.<br />Pause or cancel anytime.</p>
-        
+        <p className="text-neutral-800">
+          Fixed price
+          <br />
+          No minimum commitment.
+          <br />
+          Pause or cancel anytime.
+        </p>
+
         <ul className="mt-4 space-y-2">
           {feature.map((item, index) => (
-            <li key={index} className="flex items-center space-x-2 text-neutral-800">
+            <li
+              key={index}
+              className="flex items-center space-x-2 text-neutral-800"
+            >
               <span className="text-red-500">✔</span>
               <span>{item}</span>
             </li>
@@ -30,12 +39,8 @@ const PricingCard = ({
         </ul>
 
         <div className="mt-6 flex space-x-4">
-            <Button variant={"default"}>
-              Get Started
-            </Button>
-            <Button variant={"link"}>
-              Book a Call
-            </Button>
+          <Button variant={"default"}>Get Started</Button>
+          <Button variant={"secondary"}>Book a Call</Button>
         </div>
       </div>
       <div className="overflow-hidden w-1/2 ">

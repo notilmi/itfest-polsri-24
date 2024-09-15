caeacf57b62e4e5ca353a6a8482a72295a76dc40
Achmad Ilmi Al Akbar <113961961+notilmi@users.noreply.github.com>
2024-09-15 11:12:57 +0700

    Fix Responsiveness In Pricing Section

--- src/components/pricing-card.tsx
+++ src/components/pricing-card.tsx
@@ -1,6 +1,7 @@
 import { PricingSchema } from "@/lib/types";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { Calendar, Handshake } from "lucide-react";
 
 const PricingCard = ({
   currency,
@@ -10,8 +11,13 @@ const PricingCard = ({
   className,
 }: PricingSchema & { className: string }) => {
   return (
-    <div className="flex flex-wrap flex-row-reverse items-center justify-center  rounded-xl border">
-      <div className={cn("mx-auto w-1/2  p-8", className)}>
+    <div
+      className={cn(
+        "flex flex-col md:flex-row flex-wrap items-center justify-center rounded-xl border",
+        className
+      )}
+    >
+      <div className={cn("mx-auto w-full md:w-1/2 p-8")}>
         <h2 className="text-2xl font-bold mb-4">{type}</h2>
 
         <p className="text-5xl font-extrabold mb-2">
@@ -31,12 +37,16 @@ const PricingCard = ({
           ))}
         </ul>
 
-        <div className="mt-6 flex space-x-4">
-          <Button variant={"default"}>Get Started</Button>
-          <Button variant={"secondary"}>Book a Call</Button>
+        <div className="mt-6 flex flex-col md:flex-row gap-2">
+          <Button variant={"default"}>
+            Mulai Bekerja Sama <Handshake className="ml-2 size-4" />
+          </Button>
+          <Button variant={"secondary"}>
+            Jadwalkan Demo <Calendar className="ml-2 size-4" />
+          </Button>
         </div>
       </div>
-      <div className="overflow-hidden w-1/2 ">
+      <div className="overflow-hidden w-full md:w-1/2 ">
         <img src="/pricing-img.png" alt="Gambar" />
       </div>
     </div>

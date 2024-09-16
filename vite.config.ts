6d1d7b4aba2baebfc0b63364beeebc106a93e7cd
Achmad Ilmi Al Akbar <113961961+notilmi@users.noreply.github.com>
2024-09-16 20:10:47 +0700

    Added Static Index Single File Support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { viteSingleFile } from "vite-plugin-singlefile";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   base: "/",
   resolve: {
     alias: {
